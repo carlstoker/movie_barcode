9227f4e42d5b2eb93db181985c84f46b45b1f700
cstok <cstok@july.email>
2018-06-13 19:45:49 -0400

    resizing now done while extracting frames

--- moviebarcode.py
+++ moviebarcode.py
@@ -12,9 +12,6 @@ def process_video():
 
     with TemporaryDirectory() as settings['temp']:
         extract_frames()
-        if settings['smooth']:
-            resize_frames(1)
-            resize_frames(settings['height'])
         combine_frames()
     return None
 
@@ -22,6 +19,11 @@ def extract_frames():
     global settings
 
     print('Extracting {width} frames from {filename} to {temp} with a {interval:.02f} second interval'.format(**settings))
+    scale = []
+    if not settings['rough']:
+        scale.append('scale=1:1')
+    scale.append('scale=1:{height}'.format(**settings))
+
     for i in progressbar.progressbar(range(settings['width'])):
         capture_time = settings['start'] + ((i + 1) * settings['interval'])
 
@@ -29,25 +31,13 @@ def extract_frames():
             'ffmpeg',
             '-ss', str(capture_time),
             '-i', '{filename}'.format(**settings),
-            '-vf', 'format=yuvj444p,scale=1:{height}'.format(**settings),
+            '-vf', 'format=yuvj444p,{}'.format(','.join(scale)),
             '-vframes', '1',
             '-y',
             '-loglevel', 'fatal',
             '{}/frame{:05d}.jpg'.format(settings['temp'], i)]
         subprocess.call(command)
-
-    return None
-
-def resize_frames(height):
-    global settings
-
-    print('Resizing Frames to 1x{}'.format(height))
-    command = [
-        'mogrify',
-        '-resize', '1x{}!'.format(height),
-        '*.jpg'
-    ]
-    subprocess.call(command, cwd = settings['temp'])
+    print(command)
 
     return None
 
@@ -99,7 +89,7 @@ if __name__ == "__main__":
     parser.add_argument('--duration', help='duration of capture (in seconds) (default: %(default)s)', default=None, metavar='DURATION'),
     parser.add_argument('--height', help='barcode height (default: %(default)s)', default=1875, metavar='PIXELS', type=int)
     parser.add_argument('--output', help='output directory (default: %(default)s)', default='~/Pictures', metavar='DIR')
-    parser.add_argument('--smooth', help='enable/disable single color vertical lines (default: %(default)s)', default=True, action='store_true')
+    parser.add_argument('--rough', help='disable single color vertical lines (default: %(default)s)', default=False, action='store_true')
     parser.add_argument('--start', help='start point (in seconds) (default: %(default)s)', default=0, metavar='START')
     parser.add_argument('--width', help='barcode width/number of frames (default: %(default)s)', default=5000, metavar='PIXELS', type=int)
     parser.add_argument('--version', action='version', version='%(prog)s 1.0alpha')
