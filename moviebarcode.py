9338911ba6c9172956f2024efad3170f7a3c8cd8
cstok <40079934+cstok@users.noreply.github.com>
2019-04-14 12:36:18 -0400

    Refactor. Code cleanup.

--- moviebarcode.py
+++ moviebarcode.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Generate barcode for video input(s)."""
+
 import argparse
 import json
 import os
@@ -8,48 +10,66 @@ from tempfile import TemporaryDirectory
 import progressbar
 
 
-def generate_barcode(filename):
+def generate_barcode(filename, output_directory, start_time, duration, height, width, frame_directory, framewidth, overwrite=False, rough=False):
+    """Generate barcode from specified filename.
 
+    Arguments:
+    filename -- filename to use for barcode generation
+    output_directory -- output directory for barcode
+    start_time -- start time to start grabbing frames
+    duration -- duration to grab frames
+    height -- height of final barcode
+    width -- width of final barcode
+    frame_directory -- directory to store frames
+    framewidth -- width of each bar
+    overwrite -- overwrite previously generated barcode (default: False)
+    rough -- disable single color vertical lines (default: False)
+    """
     metadata = get_metadata(filename, ['duration'])
 
-    # Prevent missing frames at the end of the file by capping duration to 95% of video duration
-    if SETTINGS['duration'] == 0:
-        SETTINGS['duration'] = (metadata['duration'] * 0.95) - SETTINGS['start']
+    # Caps capture duration to 95% of video duration
+    # to prevent missing frames at the end of the file.
+    if duration == 0:
+        duration = (metadata['duration'] * 0.95) - start_time
     else:
-        SETTINGS['duration'] = min(
-            SETTINGS['duration'],
-            (metadata['duration'] * 0.95) - SETTINGS['start']
+        duration = min(
+            duration,
+            (metadata['duration'] * 0.95) - start_time
         )
 
-    SETTINGS.update({
-        'filename': filename,
-        'basename': os.path.splitext(os.path.basename(filename))[0],
-        'frames': int(SETTINGS['width'] / SETTINGS['framewidth'])
-    })
-    SETTINGS.update({
-        'interval': SETTINGS['duration'] / SETTINGS['frames'],
-        'barcode_filename':
-            os.path.join(SETTINGS['output'],
-                         '{basename}-{framewidth}x{frames}barcode.png'.format(**SETTINGS))
-    })
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    frames = int(width / framewidth)
+    interval = duration / frames
+    barcode_filename = os.path.join(
+        output_directory,
+        '{0}-{1}x{2}x{3}.png'.format(basename, framewidth, height, frames)
+    )
 
-    if not SETTINGS['overwrite'] and os.path.isfile(SETTINGS['barcode_filename']):
-        print('Barcode exists. Skipping {filename}'.format(**SETTINGS))
+    if not overwrite and os.path.isfile(barcode_filename):
+        print('Barcode exists. Skipping {0}'.format(filename))
         return False
 
-    with TemporaryDirectory() as SETTINGS['temp']:
-        extract_frames(SETTINGS['filename'], SETTINGS['temp'], SETTINGS['start'], SETTINGS['interval'], SETTINGS['frames'], SETTINGS['framewidth'], SETTINGS['height'], SETTINGS['rough'])
-        combine_frames(SETTINGS['temp'], SETTINGS['barcode_filename'])
+    extract_frames(
+        filename,
+        frame_directory,
+        start_time,
+        interval,
+        frames,
+        height,
+        framewidth,
+        rough
+    )
+    combine_frames(frame_directory, barcode_filename)
 
     return True
 
 
-def extract_frames(filename, output_directory, start_time, interval, frames, height, width, rough=False):
+def extract_frames(filename, frame_directory, start_time, interval, frames, height, width, rough=False):
     """Extract individual frames for processing.
 
     Arguments:
     filename -- file from which to extract frames
-    output_directory -- directory to store frames
+    frame_directory -- directory to store frames
     start_time -- start time for capturing frames
     interval -- interval between frames
     frames -- number of frames to capture
@@ -64,7 +84,7 @@ def extract_frames(filename, output_directory, start_time, interval, frames, hei
     scale.append('scale={0}:{1}'.format(width, height))
 
     print('Extracting {0} frames from {1} to {2} with a {3:.02f} second interval'.format(
-        frames, filename, output_directory, interval))
+        frames, filename, frame_directory, interval))
     for i in progressbar.progressbar(range(frames)):
         capture_time = start_time + ((i + 1) * interval)
 
@@ -76,7 +96,7 @@ def extract_frames(filename, output_directory, start_time, interval, frames, hei
             '-vframes', '1',
             '-y',
             '-loglevel', 'fatal',
-            os.path.join(output_directory, 'frame{0:05d}.png'.format(i))
+            os.path.join(frame_directory, 'frame{0:05d}.png'.format(i))
         ]
         subprocess.call(command)
 
@@ -139,44 +159,107 @@ def get_metadata(filename, keys):
 
 
 if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser(description='Generate barcode for the FILE(s)')
-    PARSER.add_argument('FILE', nargs='+')
-    PARSER.add_argument('--duration', help='duration of capture (in seconds)',
-                        default=0, metavar='DURATION', type=float)
-    PARSER.add_argument('--framewidth', help='width for each frame (default: %(default)s)',
-                        default=1, metavar='PIXELS', type=int)
-    PARSER.add_argument('--height', help='height of barcode (default: %(default)s)',
-                        default=1875, metavar='PIXELS', type=int)
-    PARSER.add_argument('--interactive',
-                        help='prompt for each file\'s new title (default: %(default)s)',
-                        default=False, action='store_true')
-    PARSER.add_argument('--output', help='output directory (default: %(default)s)',
-                        default='~/Pictures', metavar='DIR')
-    PARSER.add_argument('--overwrite', help='overwrite existing cinegrid (default: %(default)s)',
-                        default=False, action='store_true')
-    PARSER.add_argument('--prompt',
-                        help='prompt before exiting (default: %(default)s)',
-                        default=False, action='store_true')
-    PARSER.add_argument('--rough', help='disable single color vertical lines (default: %(default)s)',
-                        default=False, action='store_true')
-    PARSER.add_argument('--start', help='start point (in seconds) (default: %(default)s)',
-                        default=0, metavar='START', type=float)
-    PARSER.add_argument('--width', help='width of barcode (default: %(default)s)',
-                        default=5000, metavar='PIXELS', type=int)
-    PARSER.add_argument('--version', action='version', version='%(prog)s 1.0alpha')
+    PARSER = argparse.ArgumentParser(
+        description='Generate barcode for the FILE(s)'
+    )
+    PARSER.add_argument(
+        'FILE',
+        nargs='+'
+    )
+    PARSER.add_argument(
+        '--duration',
+        help='duration of capture (in seconds)',
+        default=0,
+        metavar='DURATION',
+        type=float
+    )
+    PARSER.add_argument(
+        '--framewidth',
+        help='width for each frame (default: %(default)s)',
+        default=1,
+        metavar='PIXELS',
+        type=int
+    )
+    PARSER.add_argument(
+        '--height',
+        help='height of barcode (default: %(default)s)',
+        default=1875,
+        metavar='PIXELS',
+        type=int)
+    PARSER.add_argument(
+        '--interactive',
+        help='prompt for each file\'s new title (default: %(default)s)',
+        default=False,
+        action='store_true'
+    )
+    PARSER.add_argument(
+        '--output',
+        help='output directory (default: %(default)s)',
+        default='~/Pictures',
+        metavar='DIR'
+    )
+    PARSER.add_argument(
+        '--overwrite',
+        help='overwrite existing cinegrid (default: %(default)s)',
+        default=False,
+        action='store_true'
+    )
+    PARSER.add_argument(
+        '--prompt',
+        help='prompt before exiting (default: %(default)s)',
+        default=False,
+        action='store_true'
+    )
+    PARSER.add_argument(
+        '--rough',
+        help='disable single color vertical lines (default: %(default)s)',
+        default=False,
+        action='store_true'
+    )
+    PARSER.add_argument(
+        '--start',
+        help='start point (in seconds) (default: %(default)s)',
+        default=0,
+        metavar='START',
+        type=float
+    )
+    PARSER.add_argument(
+        '--width',
+        help='width of barcode (default: %(default)s)',
+        default=5000,
+        metavar='PIXELS',
+        type=int
+    )
+    PARSER.add_argument(
+        '--version',
+        action='version',
+        version='%(prog)s 1.0alpha'
+    )
 
     SETTINGS = PARSER.parse_args().__dict__
     SETTINGS['output'] = os.path.expanduser(SETTINGS['output'])
     if SETTINGS['interactive']:
         SETTINGS.update({
-            'start': float(input('Enter start time (in seconds) [0]: ') or 0),
-            'duration': float(input('Enter duration (in seconds) [0]: ') or 0),
-            'height': int(input('Enter height (in pixels) [1875]: ') or 1875),
-            'width': int(input('Enter width (in pixels) [5000]: ') or 5000),
-            'framewidth': int(input('Enter frame width (in pixels) [1]: ') or 1)
+            'start': float(input('Enter start time (seconds) [0]: ') or 0),
+            'duration': float(input('Enter duration (seconds) [0]: ') or 0),
+            'height': int(input('Enter height (pixels) [1875]: ') or 1875),
+            'width': int(input('Enter width (pixels) [5000]: ') or 5000),
+            'framewidth': int(input('Enter frame width (pixels) [1]: ') or 1)
         })
 
     for file in SETTINGS['FILE']:
-        generate_barcode(file)
+        with TemporaryDirectory() as temp:
+            generate_barcode(
+                file,
+                SETTINGS['output'],
+                SETTINGS['start'],
+                SETTINGS['duration'],
+                SETTINGS['height'],
+                SETTINGS['width'],
+                temp,
+                SETTINGS['framewidth'],
+                SETTINGS['overwrite'],
+                SETTINGS['rough']
+            )
     if SETTINGS['prompt']:
         input('Press Enter to continue.')
